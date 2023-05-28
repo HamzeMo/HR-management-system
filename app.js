3e39b32ebdd7ff2091e823a9127d66cb2fb9590f
HamzeMo <isoka2h@gmail.com>
2023-05-28 06:58:56 +0000

    hr

--- app.js
+++ app.js
@@ -1,113 +1,77 @@
-var idgen = (function(){var id=1000;return function(){if(arguments[0]===0)id=0;return id++;}})();
-const employees = [
-    {
-      id: idgen(),
-      fullName: "Ghazi Samer",
-      department: "Administration",
-      level: "Senior",
-      imageUrl: "https://thumbs.dreamstime.com/b/man-handsome-mature-employee-wear-formal-suit-business-career-concept-man-handsome-mature-employee-wear-formal-suit-business-193163038.jpg",
-      generateEmployeeId: function() {
-        return idgen();
-      },
-    },
-    {
-      id: idgen(),
-      fullName: "Lana Ali",
-      department: "Finance",
-      level: "Senior",
-      imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReOx5_EINdfM9xQxjABxzPBjoIPDpgcR5dteiWXP1Kqk6RZ5dGKGCgXn-XThL0gLVYzyo&usqp=CAU.jpg",
-      generateEmployeeId: function() {
-        return idgen();
-      },
-    },
-    {
-      id: idgen(),
-      fullName: "Tamara Ayoub",
-      department: "Marketing",
-      level: "Senior",
-      imageUrl: "https://img.freepik.com/premium-photo/happy-female-office-employee-formal-white-shirt-expressing-confidence-standing-with-arms-crossed-while-thinking-about-work-day-isolated-light-wall-confident-business-woman-posing-studio_95891-4559.jpg",
-      generateEmployeeId: function() {
-        return idgen();
-      },
-    },
-    {
-      id: idgen(),
-      fullName: "Safi Walid",
-      department: "Administration",
-      level: "Mid-Senior",
-      imageUrl: "https://img.freepik.com/premium-photo/mature-employee-business-suit-white-background_474717-70122.jpg",
-      generateEmployeeId: function() {
-        return idgen();
-      },
-    },
-    {
-      id: idgen(),
-      fullName: "Omar Zaid",
-      department: "Development",
-      level: "Senior",
-      imageUrl: "https://thumbs.dreamstime.com/b/handsome-confident-businessman-standing-his-hands-pockets-42538103.jpg",
-      generateEmployeeId: function() {
-        return idgen();
-      },
-    },
-    {
-      id: idgen(),
-      fullName: "Rana Saleh",
-      department: "Development",
-      level: "Junior",
-      imageUrl: "https://www.shutterstock.com/image-photo/young-asian-business-woman-smiling-260nw-2160057221.jpg",
-      generateEmployeeId: function() {
-        return idgen();
-      },
-    },
-    {
-      id: idgen(),
-      fullName: "Hadi Ahmad",
-      department: "Finance",
-      level: "Mid-Senior",
-      imageUrl: "https://www.shutterstock.com/image-photo/smiling-young-business-man-wearing-260nw-285687404.jpg",
-      generateEmployeeId: function() {
-        return idgen();
-      },
-    },
-  ];
-  
-  function calculateSalary(level) {
-    let min, max;
-    switch (level) {
-      case "Senior":
-        min = 1500;
-        max = 2000;
-        break;
-      case "Mid-Senior":
-        min = 1000;
-        max = 1500;
-        break;
-      case "Junior":
-        min = 500;
-        max = 1000;
-        break;
-      default:
-        min = 0;
-        max = 0;
+function Employees(name, departments, levels, image) {
+
+    this.employeeId = EmployeesID();
+    this.fullName = name;
+    this.department = departments;
+    this.level = levels;
+    this.ImageUrl = image;
+    this.salary = Salary(levels);
+
+
+    function EmployeesID() {
+        return Math.floor(Math.random() * 9999) + 1000;
     }
-    const randomSalary = Math.floor(Math.random() * (max - min + 1)) + min;
-    const netSalary = randomSalary - randomSalary * 0.075;
-    return { randomSalary, netSalary };
-  }
-  
-  
-  function renderEmployees() {
-    const output = document.getElementById("output");
-    employees.forEach((employee) => {
-      const { fullName, level } = employee;
-      const { netSalary } = calculateSalary(level);
-      output.innerHTML += `Employee name: ${fullName}<br>Employee salary: ${netSalary}<br><br>`;
-    });
-    window.addEventListener("DOMContentLoaded", renderEmployees);
 
-  }
+    function Salary(level) {
+        let salaryTable = {
+            Junior: { min: 500, max: 1000 },
+            'Mid-Senior': { min: 1000, max: 1500 },
+            Senior: { min: 1500, max: 2000 }
+        };
 
-  
-  window.addEventListener("DOMContentLoaded", renderEmployees);
-  
+        let { min, max } = salaryTable[level];
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+let divE=document.createElement("div");
+let body=document.getElementsByTagName("body")[0];
+body.appendChild(divE);
+
+let empImg=document.createElement("img");
+empImg.src=this.ImageUrl;
+empImg.alt=this.fullName;
+empImg.style.maxWidth = "80%";
+divE.appendChild(empImg);
+
+let employeeName=document.createElement("p");
+employeeName.textContent=`Name: ${this.fullName} - ID: ${this.employeeId}`;
+divE.appendChild(employeeName);
+
+let departmentEmp=document.createElement("p");
+departmentEmp.textContent=`Department: ${this.department} - Level: ${this.level}`;
+divE.appendChild(departmentEmp);
+
+let empSalary=document.createElement("p");
+empSalary.textContent= this.salary;
+divE.appendChild(empSalary);
+
+divE.style.border="5px solid #e6e6e6";
+divE.style.background= "#e6e6e6";
+divE.style.borderRadius= "15px";
+divE.style.padding="30px"
+divE.style.textAlign="center"
+divE.style.fontWeight="bold"
+divE.style.width="280px";
+divE.style.fontFamily="Arial, Helvetica, sans-serif"
+divE.style.fontSize = "15px";
+divE.style.margin= "40px"
+divE.style.display= "inline-block";
+divE.style.flexWrap= "wrap";
+
+}
+
+let Ghazi = new Employees("Ghazi Samer","Administration","Senior","./assets/Ghazi.jpg");
+let Lana = new Employees("Lana Ali","Finance","Senior", "./assets/Lana.jpg");
+let Tamara = new Employees("Tamara Ayoub","Marketing","Senior", "./assets/Tamara.jpg");
+let Safi = new Employees("Safi Walid","Administration","Mid-Senior", "./assets/Safi.jpg");
+let Omar = new Employees("Omar Zaid", "Development", "Senior", "./assets/Omar.jpg");
+let Rana = new Employees("Rana Saleh", "Development","Junior", "./assets/Rana.jpg");
+let Hadi = new Employees("Hadi Ahmad","Finance","Mid-Senior", "./assets/Hadi.jpg");
+
+console.log(Ghazi);
+console.log(Lana);
+console.log(Tamara);
+console.log(Safi);
+console.log(Omar);
+console.log(Rana);
+console.log(Hadi);
